e515c22ade8a1f88152c9086541e090dda35cc02
Patrick Wang <kk1fff@patrickz.net>
2012-12-07 19:00:01 +0800

    Add el-get and lua-mode

--- install.el
+++ install.el
@@ -1,11 +1,29 @@
 (require 'package)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Set up 3rd party sources.
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
+
+;; Refresh
 (package-initialize)
 (package-refresh-contents)
 
-(defvar my-packages '(clojure-mode))
+;; Define the package list.
+(defvar my-packages '())
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Install el-get package manager.
+(url-retrieve
+ "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
+ (lambda (s)
+   (let (el-get-master-branch)
+     (goto-char (point-max))
+     (eval-print-last-sexp))))
+
+;; Install lua-mode
+(el-get-install 'lua-mode)
