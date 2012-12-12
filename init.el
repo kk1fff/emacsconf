defd185e47785649b0d91a46e179cd91f15b2225
Patrick Wang <kk1fff@patrickz.net>
2012-12-12 19:59:26 +0800

    Use gtags' builtin el

--- init.el
+++ init.el
@@ -1,11 +1,14 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Customize variables
 (custom-set-variables
  '(inhibit-startup-screen t)
  '(recentf-max-saved-items 100))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Highlight current line
 (global-hl-line-mode 1)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; [Additional Package] F3 to highlight current symbol.
 (require 'highlight-symbol)
 (global-set-key [(control f3)] 'highlight-symbol-at-point)
@@ -13,6 +16,7 @@
 (global-set-key [(shift f3)] 'highlight-symbol-prev)
 (global-set-key [(meta f3)] 'highlight-symbol-prev)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Show file full path in title bar
 (setq-default frame-title-format
    (list '((buffer-file-name " %f"
@@ -21,12 +25,15 @@
               (revert-buffer-function " %b"
               ("%b - Dir:  " default-directory)))))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Shows parenthesis 
 (show-paren-mode 1)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Shows column number, line number.
 (column-number-mode 1)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; disable toolbar
 (progn
   (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
@@ -35,6 +42,7 @@
 )
 (setq-default truncate-lines t)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; [Additional Package] Use column indent
 (require 'fill-column-indicator)
 (setq fci-rule-width 1)
@@ -42,19 +50,23 @@
 (add-hook 'after-change-major-mode-hook 'fci-mode)
 (setq-default fill-column 80)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Show line number
 (global-linum-mode 1)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; highlight 80 column
 ; (require 'column-marker)
 ; (add-hook 'after-change-major-mode-hook
 ;          (lambda () (interactive) (column-marker-2 80)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; [Additional Package] Autocomplete
 (require 'auto-complete-config)
 (ac-config-default)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Coding style
 ;; Using google style
 ;; We're at Mozilla, so we don't use Google's style.
@@ -62,10 +74,12 @@
 ; (add-hook 'c-mode-common-hook 'google-set-c-style)
 ; (add-hook 'c-mode-common-hook 'google-make-newline-indent)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Mozilla's style
 (require 'mozilla-c-style)
 (add-hook 'c-mode-common-hook 'mozilla-c-mode-style-hook)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; My additional style
 (setq-default basic-offset 2)
 (setq-default tab-width 2)
@@ -74,15 +88,18 @@
 (setq-default js-indent-level 2)
 (c-set-offset 'innamespace 0)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Use c++-mode as default mode of .h file
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Markdown
 (autoload 'markdown-mode "markdown-mode.el" 
   "Major mode for editing Markdown files" t)
 (setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Popup kill ring.
 (require 'popup)
 (require 'pos-tip)
@@ -90,6 +107,7 @@
 (global-set-key "\M-y" 'popup-kill-ring)
 (setq popup-kill-ring-interactive-insert t)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Add revert-all-buffers command.
 (defun revert-all-buffers ()
     "Refreshes all open buffers from their respective files."
@@ -100,36 +118,62 @@
           (revert-buffer t t t) )))
     (message "Refreshed open files.") )
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Enable history
 (require 'recentf)
 (recentf-mode 1)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Enable js-mode when opening a jsm file
 (setq auto-mode-alist (cons '("\\.jsm" . js-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.sjs" . js-mode) auto-mode-alist))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Load color theme
 (require 'color-theme)
 (color-theme-initialize)
 (color-theme-hober)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Load minimap
 ; (require 'minimap)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; gtags
-(require 'xgtags)
-(xgtags-mode 1)
+; (require 'xgtags)
+; (xgtags-mode 1)
+(require 'gtags)
+(gtags-mode 1)
+; (defun ww-next-gtag ()
+;   "Find next matching tag, for GTAGS."
+;   (interactive)
+;   (let ((latest-gtags-buffer
+;          (car (delq nil  (mapcar (lambda (x) (and (string-match "GTAGS SELECT" (buffer-name x)) (buffer-name x)) )
+;                                  (buffer-list)) ))))
+;     (cond (latest-gtags-buffer
+;            (switch-to-buffer latest-gtags-buffer)
+;            (forward-line)
+;            (gtags-select-it nil))
+;           ) ))
+; (global-set-key "\M-;" 'ww-next-gtag)   ;; M-; cycles to next result, after doing M-. C-M-. or C-M-,
+; (global-set-key "\M-." 'gtags-find-tag) ;; M-. finds tag
+; (global-set-key [(control meta .)] 'gtags-find-rtag)   ;; C-M-. find all references of tag
+; (global-set-key [(control meta ,)] 'gtags-find-symbol) ;; C-M-, find all usages of symbol.
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; emacs-nav
 (require 'nav)
 (nav-disable-overeager-window-splitting)
 ;; Optional: set up a quick key to toggle nav
 (global-set-key [f8] 'nav-toggle)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Popup global mark ring.
 (require 'popup-global-mark-ring)
 (global-set-key "\C-c\C-g" 'popup-global-mark-ring)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Move to minibuffer
 (defun switch-to-minibuffer-window ()
   "switch to minibuffer window (if active)"
