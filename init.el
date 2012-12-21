595870162135f67be5709e2d6fcdcc4be8ad315b
Patrick Wang <kk1fff@patrickz.net>
2012-12-21 12:16:02 +0800

    Make gtags re-index the file that is just saved

--- init.el
+++ init.el
@@ -140,10 +140,41 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; gtags
-; (require 'xgtags)
-; (xgtags-mode 1)
 (require 'gtags)
 (gtags-mode 1)
+
+; Enabling single file update.
+(defun gtags-root-dir ()
+  "Returns GTAGS root directory or nil if doesn't exist."
+  (with-temp-buffer
+    (if (zerop (call-process "global" nil t nil "-pr"))
+        (buffer-substring (point-min) (1- (point-max)))
+      nil)))
+
+(defun gtags-update ()
+  "Make GTAGS incremental update"
+  (call-process "global" nil nil nil "-u"))
+
+(defun gtags-update-single(filename)  
+  "Update Gtags database for changes in a single file"
+  (interactive)
+  (start-process "update-gtags" "update-gtags" "bash" "-c" (concat "cd " (gtags-root-dir) " ; gtags --single-update " filename )))
+
+(defun gtags-update-current-file()
+  (interactive)
+  (defvar filename)
+  (setq filename (replace-regexp-in-string (gtags-root-dir) "." (buffer-file-name (current-buffer))))
+  (gtags-update-single filename)
+  (message "Gtags updated for %s" filename))
+
+(defun gtags-update-hook()
+  "Update GTAGS file incrementally upon saving a file"
+  (when gtags-mode
+    (when (gtags-root-dir)
+      (gtags-update-current-file))))
+
+(add-hook 'after-save-hook 'gtags-update-hook)
+
 ; (defun ww-next-gtag ()
 ;   "Find next matching tag, for GTAGS."
 ;   (interactive)
