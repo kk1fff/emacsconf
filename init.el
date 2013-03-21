f340edfaabc7feec02b7e181ba5d5e3882dcfe4e
Patrick Wang <kk1fff@patrickz.net>
2013-03-21 17:40:23 +0800

    Add ace-jump-mode

--- init.el
+++ init.el
@@ -255,3 +255,13 @@
 
 ;; id-utils
 ; (require 'idutils "id-utils")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Ace Jump Mode
+(autoload
+  'ace-jump-mode
+  "ace-jump-mode"
+  "Emacs quick move minor mode"
+  t)
+;; you can select the key you prefer to
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
