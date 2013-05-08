313c7b8683a11e042ad675a1eb4d5fae30c2bba2
Patrick Wang <kk1fff@patrickz.net>
2013-05-08 18:02:47 +0800

    add C-M-Left|Right to key bind to switch between windows

--- init.el
+++ init.el
@@ -271,3 +271,16 @@
 ; (and
 ;  (require 'centered-cursor-mode)
 ;  (global-centered-cursor-mode +1))
+
+(defun select-next-window ()
+  "Switch to the next window"
+  (interactive)
+  (select-window (next-window)))
+
+(defun select-previous-window ()
+  "Switch to the previous window"
+  (interactive)
+  (select-window (previous-window)))
+
+(global-set-key (kbd "C-M-<right>") 'select-next-window)
+(global-set-key (kbd "C-M-<left>")  'select-previous-window)
