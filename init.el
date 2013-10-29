b10908dc963a69cf3bea96c05d3fec59afe80a63
Patrick Wang <kk1fff@patrickz.net>
2013-10-29 12:07:27 +0800

    use C-c / as highlight symbol, C-c , as previous symbol, C-c . as next symbol

--- init.el
+++ init.el
@@ -20,10 +20,9 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; [Additional Package] F3 to highlight current symbol.
 (require 'highlight-symbol)
-(global-set-key [(control f3)] 'highlight-symbol-at-point)
-(global-set-key [f3] 'highlight-symbol-next)
-(global-set-key [(shift f3)] 'highlight-symbol-prev)
-(global-set-key [(meta f3)] 'highlight-symbol-prev)
+(global-set-key (kbd "C-c /") 'highlight-symbol-at-point)
+(global-set-key (kbd "C-c ,") 'highlight-symbol-prev)
+(global-set-key (kbd "C-c .") 'highlight-symbol-next)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Show file full path in title bar
