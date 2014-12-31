827e89c8c1ce87185f1a2335ee3df5217d5a1eae
Chih-Kai (Patrick) Wang <kk1fff@ckwang.info>
2015-01-01 03:30:01 +0800

    remove pushd and popd from update-autocomplete.sh

--- update-autocomplete.sh
+++ update-autocomplete.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 TEMP=$(mktemp -d)
+BASE=$PWD
 
-pushd $TEMP
+cd $TEMP
 git clone https://github.com/cask/cask.git cask
 git clone https://github.com/auto-complete/auto-complete.git 
 
 cd auto-complete
 PATH=$PATH:$TEMP/cask/bin make byte-compile
 
-popd
+cd $BASE
 if [ -d auto-complete ]; then
     rm -rf auto-complete
 fi
